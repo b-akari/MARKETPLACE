8a49795d73c1b0c0748f4de0f335c31eb0996f6e
b-akari <108397871+b-akari@users.noreply.github.com>
2023-01-12 15:25:10 +0800

    modified

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
 import express from "express"
-
+import mysql from "mysql"
 const app= express()
+
+
+const db= mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Onedirection28_",
+    database: "marketplace"
+})
+
+app.get("/", (req, res)=>{
+    res.json("hello, this is the backend")
+})
+
+app.get("/shoes", (req, res)=>{
+    const q= "SELECT * FROM shoes"
+    db.query(q, (err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
 
 app.listen(8800, ()=> {
     console.log("connected to backend")
