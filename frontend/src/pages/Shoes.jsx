2c808c557b5e0bc6eec39f90615470d490017c58
b-akari <108397871+b-akari@users.noreply.github.com>
2023-02-03 00:51:28 +0800

    modified

--- frontend/src/pages/Shoes.jsx
+++ frontend/src/pages/Shoes.jsx
@@ -1,53 +1,70 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Card, Grid } from "@mui/material";
 
-const Shoes = ()=>{
-    
-    const[shoes, setShoes]=useState([])
+let PHPPrice = new Intl.NumberFormat("en-PH", {
+  style: "currency",
+  currency: "PHP",
+});
 
-    useEffect(()=>{
-        const fetchAllShoes= async()=>{
-            try {
-                const res= await axios.get("http://localhost:8080/shoes")
-                setShoes(res.data)
-            } catch(err){
-                console.log(err)
-            }
-        }
-        fetchAllShoes()
-    },[])
+const Shoes = () => {
+  const [shoes, setShoes] = useState([]);
 
-    const handleDelete= async(id)=>{
-        try {
-            await axios.delete("http://localhost:8080/shoes/" + id)
-            window.location.reload()
-        } catch(err){
-            console.log(err)
-        }
+  useEffect(() => {
+    const fetchAllShoes = async () => {
+      try {
+        const res = await axios.get("http://localhost:8080/shoes");
+        setShoes(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllShoes();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete("http://localhost:8080/shoes/" + id);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return(
-        <div>
-            <h1>Shoe Store</h1>
-            <div className='shoes'>
-                {shoes.map((shoe)=>(
-                    <div className='shoe' key={shoe.id}>
-                    {shoe.image && <img src={`/image/${shoe.image}`} alt=""/>}
-                    <h2> {shoe.prod_name}</h2>
-                    <p> {shoe.prod_description}</p>
-                    <span>{shoe.price}</span>
-                    <button className='delete' onClick={()=> handleDelete(shoe.id)}>Delete</button>
-                    <button className='Update'> <Link to={`/update/${shoe.id}`}>Update</Link> </button>
-                    </div>
-                ))}
-            </div>
-        <button>
-            <Link to= "/add">Add new item</Link>
-        </button>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <h1>THE GARDEN</h1>
+      <div className="shoes">
+        {shoes.map((shoe) => (
+          <Grid key={shoe.id} item xs={12} sm={6} md={3}>
+            <Card sx={{ background: "#EBECF0" }}>
+              <div className="shoe" key={shoe.id}>
+                {shoe.image && <img src={`/image/${shoe.image}`} alt="" />}
+                <h2>{shoe.prod_name}</h2>
+                <p>{shoe.prod_description}</p>
+                <h5>{PHPPrice.format(shoe.price)}</h5>
+                <button
+                  className="delete"
+                  onClick={() => handleDelete(shoe.id)}
+                >
+                  Delete
+                </button>
+                <button className="update">
+                  <Link to={`/update/${shoe.id}`}>Update</Link>
+                </button>
+              </div>{" "}
+            </Card>{" "}
+          </Grid>
+        ))}
+      </div>
 
-export default Shoes
+      <button className="add">
+        <Link to="/add">Add new Shoes</Link>
+      </button>
+    </div>
+  );
+};
+
+export default Shoes;
