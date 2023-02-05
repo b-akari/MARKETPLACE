3857f33ec131f4aebf0c7fa977c7f257852178f6
b-akari <108397871+b-akari@users.noreply.github.com>
2023-02-05 20:58:13 +0800

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Typography } from "@mui/material";
+
 
 const Home = () => {
   return (
     <div className="bg">
-      <Typography variant="h2">HOME PO ITO</Typography>
+      
     </div>
   );
 };
