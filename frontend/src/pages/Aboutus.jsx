2acd2687c97452014e10a63b4938015af66b5099
b-akari <108397871+b-akari@users.noreply.github.com>
2023-02-07 14:21:10 +0800

    .

--- frontend/src/pages/Aboutus.jsx
+++ frontend/src/pages/Aboutus.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const Aboutus = () => {
   return (
     <div className="about-us">
-      <div>
-        <img></img>
+      <div
+        style={{
+          backgroundColor: "#82AAE3",
+          padding: "50px",
+          borderRadius: "50px",
+        }}
+      >
+        <img
+          src="./image/me.jpg"
+          style={{ borderRadius: "50%", height: "200px", width: "200px" }}
+        />
+        <Typography variant="h6" sx={{mt: 2, color: "white", fontWeight: 500}}>
+          Ana Bridgete P. Seguban <br />
+          Creator
+        </Typography>
       </div>
     </div>
   );
